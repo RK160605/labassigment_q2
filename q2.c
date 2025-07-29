2b8003abd6a64a811f27736249b1089cc8049117
RK160605 <rishabkoul166@gmail.com>
2025-07-29 13:32:34 +0530

    Create q2.c

---
+++ q2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int sum = 0;
+    float average;
+
+  
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+   
+    int arr[n]; 
+
+   
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+        sum += arr[i]; 
+    }
+
+    
+    
+    average = (float)sum / n; 
+
+   
+    printf("The average of the integers is: %.2f\n", average);
+
+    return 0;
+}
